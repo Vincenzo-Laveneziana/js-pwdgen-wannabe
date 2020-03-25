03fe5115a8f70241838650b0c26db0ebef114cd1
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-03-25 18:21:13 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 
 //chiedere all’utente il suo nome e poi memorizzarlo
-var nome=prompt("Inserisci il tuo nome")
+var nome=prompt("Inserisci il tuo nome", "Nome");
 console.log(nome);
 
+
 //chiedere poi il suo cognome e memorizzarlo
-var cognome=prompt("Inserisci il tuo cognome")
+var cognome=prompt("Inserisci il tuo cognome", "Cognome");
 console.log(cognome);
 
 //Visualizza nome e cognome in h3
@@ -13,12 +14,12 @@ document.getElementById("utente").innerHTML="Hello "+ nome +" "+ cognome + "!!!"
 
 //chiedere poi il suo colore preferito e memorizzarlo
 
-var colore=prompt("Inserisci il tuo colore preferito")
+var colore=prompt("Inserisci il tuo colore preferito" ,"Blu");
 console.log(colore);
 
 //inserisco variabile dichiarata
 var intero= 20;
 
-//in fine scrivere nella pagina le informazioni ottenuti dall’utente, nel formato: 
+//in fine scrivere nella pagina le informazioni ottenuti dall’utente, nel formato:
 
 document.getElementById("visualizza").innerHTML= nome + cognome + colore + intero;
